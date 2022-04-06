7ef6227ecec25286d66e03b3e5504d78386f3dfb
Uskompuf <22492406+Uskompuf@users.noreply.github.com>
2022-04-06 15:57:42 +1000

    updates

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -1,2 +1,13 @@
 import { defineElements } from '@runno/runtime'
+
+// Fix for Runno
+window.process = window.process || {
+    platform: null,
+    browser: true,
+    env: {
+      NODE_DEBUG: false,
+    }
+};
+
+// Initialise Runno
 defineElements();
