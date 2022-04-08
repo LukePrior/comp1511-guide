e0f6034a69c6d1eeb6144f464c30d9282a2bb64b
Uskompuf <22492406+Uskompuf@users.noreply.github.com>
2022-04-08 16:13:04 +1000

    should work offline now maybe

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,6 @@ const precachedAssets = [
 
 // Assets to permenantly precache
 const permenantCachedAssets = [
-    'https://registry.wapm.io/graphql',
     'https://registry-cdn.wapm.io/packages/taybenlor/runno-clang/runno-clang-0.1.2.tar.gz'
 ];
 
@@ -24,16 +23,15 @@ const graphQLResult = {"data":{"command":{"command":"runno-clang","module":{"nam
 // Precache assets on install
 self.addEventListener('install', (event) => {
     event.waitUntil(caches.open(cacheName).then((cache) => {
-        //const cacheList = precachedAssets.concat(permenantCachedAssets);
-        //console.log(cacheList);
         fillServiceWorkerCache();
     }));
   });
 
 const fillServiceWorkerCache = function () {
+    const cacheList = precachedAssets.concat(permenantCachedAssets);
     return caches.open(cacheName).then(function (cache) {
         return Promise.all(
-            precachedAssets.map(function (url) {
+            cacheList.map(function (url) {
                 return cache.add(url)
             })
         );
@@ -44,15 +42,19 @@ const fillServiceWorkerCache = function () {
 self.addEventListener('fetch', function (event) {
     const url = new URL(event.request.url);
     const isPrecachedRequest = precachedAssets.includes(url.pathname);
-    //const isPermenantPrecachedRequest = permenantCachedAssets.includes(url.pathname);
-    // ToDo refesh isPrecachedRequest
+    const isPermenantPrecachedRequest = permenantCachedAssets.includes(url.pathname);
     if (isPrecachedRequest) {
         event.respondWith(caches.open(cacheName).then((cache) => {
             console.log("Serve: " + event.request.url);
             return cache.match(event.request.url);
         }));
-    } else if (event.request.url == graphQL) {
-        console.log("GraphQL: " + event.request.url);
+    } else if (isPermenantPrecachedRequest) {
+        event.respondWith(caches.open(cacheName).then((cache) => {
+            console.log("Serve: " + event.request.url);
+            return cache.match(event.request.url);
+        }));
+    } else if (event.request.url == graphQL) { //GraphQL
+        console.log("Serve: " + event.request.url);
         return graphQLResult;
     } else {
         console.log("Fallback: " + event.request.url);
