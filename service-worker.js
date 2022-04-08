f2e3a73db23c6d49dafe49f03fbb01e0a8738ce4
LukePrior <22492406+LukePrior@users.noreply.github.com>
2022-04-08 05:48:15 +0000

    Deploying to gh-pages from @ LukePrior/comp1511-guide@567630da2c94a1af3be3fd32ed1d285eacf87aab 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,41 @@
+// Establish cache
+const cacheName = 'main_cache';
+
+// Assets to precache but reload
+const precachedAssets = [
+    '/static/css/main.css',
+    '/static/js/index.js',
+    '/manifest.json',
+    '/static/img/favicon.ico',
+    '/static/img/android-chrome-192x192.png',
+    '/static/img/android-chrome-512x512.png',
+    '/static/img/apple-touch-icon.png',
+];
+
+// Assets to permenantly precache
+const permenantCachedAssets = [
+    'https://registry.wapm.io/graphql',
+    'https://registry-cdn.wapm.io/packages/taybenlor/runno-clang/runno-clang-0.1.2.tar.gz'
+];
+
+// Precache assets on install
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(cacheName).then((cache) => {
+        const cacheList = precachedAssets.concat(permenantCachedAssets);
+        return cache.addAll(cacheList);
+    }));
+  });
+  
+
 self.addEventListener('fetch', function (event) {
-    // do nothing here, just log all the network requests
+    const url = new URL(event.request.url);
+    const isPrecachedRequest = precachedAssets.includes(url.pathname);
+    const isPermenantPrecachedRequest = permenantCachedAssets.includes(url.pathname);
+    // ToDo refesh isPrecachedRequest
+    if (isPrecachedRequest || isPermenantPrecachedRequest) {
+        event.respondWith(caches.open(cacheName).then((cache) => {
+            return cache.match(event.request.url);
+        }));
+    }
     console.log(event.request.url);
   });
